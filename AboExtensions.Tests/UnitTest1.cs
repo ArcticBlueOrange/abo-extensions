a47ec89d388cd5384032338a0a3ef5f817dd74bc
ArcticBlueOrange <brando1411@gmail.com>
2026-06-05 09:37:21 +0200

    first commit

---
+++ AboExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,67 @@
+using AboExtensions.Strings;
+
+namespace AboExtensions.Tests;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("", true)]
+    [InlineData(null, true)]
+    [InlineData("mario", false)]
+    [InlineData(" mario", false)]
+    [InlineData("luigi", false)]
+    [InlineData("luigi ", false)]
+    [InlineData(" ", true)]
+    [InlineData("\t", true)]
+    [InlineData("\n", true)]
+    public void TestNWS(string? s, bool exp)
+    {
+        var res = s.IsNullOrWhiteSpace();
+        Assert.Equal(exp, res);
+    }
+    [Theory]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    [InlineData("mario", true)]
+    [InlineData(" mario", true)]
+    [InlineData("luigi", true)]
+    [InlineData("luigi ", true)]
+    [InlineData(" ", false)]
+    [InlineData("\t", false)]
+    [InlineData("\n", false)]
+    public void TestNNWS(string? s, bool exp)
+    {
+        var res = s.IsNotNullOrWhiteSpace();
+        Assert.Equal(exp, res);
+    }
+    [Theory]
+    [InlineData("", true)]
+    [InlineData(null, true)]
+    [InlineData("mario", false)]
+    [InlineData(" mario", false)]
+    [InlineData("luigi", false)]
+    [InlineData("luigi ", false)]
+    [InlineData(" ", false)]
+    [InlineData("\t", false)]
+    [InlineData("\n", false)]
+    public void TestNB(string? s, bool exp)
+    {
+        var res = s.IsNullOrEmpty();
+        Assert.Equal(exp, res);
+    }
+    [Theory]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    [InlineData("mario", true)]
+    [InlineData(" mario", true)]
+    [InlineData("luigi", true)]
+    [InlineData("luigi ", true)]
+    [InlineData(" ", true)]
+    [InlineData("\t", true)]
+    [InlineData("\n", true)]
+    public void TestNNB(string? s, bool exp)
+    {
+        var res = s.IsNotNullOrEmpty();
+        Assert.Equal(exp, res);
+    }
+}
---
+++ AboExtensions/Strings/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboExtensions.Strings;
+
+public static class StringExtensions
+{
+    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? s) =>
+        string.IsNullOrWhiteSpace(s);
+    public static bool IsNotNullOrWhiteSpace([NotNullWhen(true)] this string? s) =>
+        !string.IsNullOrWhiteSpace(s);
+    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? s) =>
+        string.IsNullOrEmpty(s);
+    public static bool IsNotNullOrEmpty([NotNullWhen(true)] this string? s) =>
+        !string.IsNullOrEmpty(s);
+}
