d34abe03bcba04ac3cef93ab0c3e77e7d9451745
ArcticBlueOrange <brando1411@gmail.com>
2026-06-05 13:51:39 +0200

    Create Reflections.cs

---
+++ AboExtensions/Reflections/Reflections.cs
@@ -0,0 +1,29 @@
+﻿using AboExtensions.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboExtensions.Reflections;
+
+public static class Reflections
+{
+    public static object GetPropertyByName(this object o, string property)
+    {
+        return o.GetType().GetProperty(property).GetValue(o, null);
+    }
+    public static string GetPropertiesToString(object o, string propNames, char inSep = ',', char outSep = ' ')
+    {
+        if (!propNames.Contains(inSep))
+            return GetPropertyByName(o, propNames).ToString();
+
+        var props = propNames.Split(inSep)
+            .Where(s => !s.IsNullOrWhiteSpace())
+            .Select(s => GetPropertyByName(o, s))
+            .ToList();
+
+        return string.Join(outSep, props);
+    }
+}
+
